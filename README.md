a85d4246f8a95b3609d3ab311e95fe03a3ba0f3b
Doug Sievers <dwsievers@gmail.com>
2014-01-25 21:03:10 -0800

    Update comments and README

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ opencv_examples
 ===============
 
 Some test files for working with OpenCV
+
+Press any key to continue through edge_detector.py
+
+Tested on Ubuntu 12.04.4 LTS
+* Python 2.7.3
+* NumPy 1.6.1
+* SciPy 0.9.0
+* OpenCV 2.4.6
+
--- edge_detector.py
+++ edge_detector.py
@@ -6,20 +6,27 @@ from scipy import stats
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-
 #Use a black and white image
 
 image = cv2.imread('bw.png')
 cv2.imshow("Image", image)
 cv2.moveWindow("Image", 0, 0)
 
+# find edges using derivative in X/Y at the same time
+# doesn't work well on round shapes
+# side note : from black to white, derivative is +255, no problem
+#             but white to black is -255, which OpenCV changes to 1
+#             I fixed these using the stats.threshold method to make
+#             all values > 0 to 255
 edges = image[1:,1:,:] - image[:-1,:-1,:]
 edges = stats.threshold(edges, threshmax=0, newval=255)
 cv2.imshow("Partial Edges", edges)
 cv2.moveWindow("Partial Edges", 450, 0)
 
+# find edges using X derivate and Y derivative separately
 edgesH = image[1:,:,:] - image[:-1,:,:]
 edgesV = image[:,1:,:] - image[:,:-1,:]
+# add the matrices together. we have to crop a row/column for it to match size
 edgesA = edgesH[:,:-1,:] + edgesV[:-1,:,:]
 edgesA = stats.threshold(edgesA, threshmax=0, newval=255)
 cv2.imshow("All Edges", edgesA)
@@ -71,7 +78,7 @@ cv2.moveWindow("All Edges", 900, 0)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-
+#matplotlib commands, not using right now
 #plt.imshow(image)
 #plt.show()
 
